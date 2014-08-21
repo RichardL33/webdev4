ccd3a549933196304e6816d69640787a6c58004a
richard l <richardlee454@gmail.com>
2014-08-20 23:45:14 -0400

    Update

---
+++ webapp/app/scripts/main.js
@@ -0,0 +1,25 @@
+$(document).on('ready', function(e) {
+	'use strict';
+
+	console.log(e);
+
+// 	var items = ['red', 'blue', 'green'];
+
+// 	for (var i -0; i < items.length; i++) {
+// 		console.log(items[i]);
+// 	};
+
+// 	var item = _.filter(items, function(val) {
+
+// 	return val === 'green';
+
+// });
+// 	var locations = data.locations;
+// 	var cityofinterest = _.filter(locations, function(loc) {
+// 		return loc === ('Atlanta');
+// 	});
+
+// 	console.log(cityofinterest);
+// 	console.log(item);
+
+});
